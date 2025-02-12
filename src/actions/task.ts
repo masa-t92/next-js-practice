42a3887da0c812ffbd6c0d40b7613b7a647c52d7
masa-t <halfmoonra@gmail.com>
2025-02-12 22:05:13 +0900

    errorを無理やり使う

--- src/actions/task.ts
+++ src/actions/task.ts
@@ -20,6 +20,7 @@ export const createTask = async (state: FormState, formData: FormData) => {
         await connectDb()
         await TaskModel.create(newTask)
     } catch (error) {
+        console.log(error);
         state.error = 'タスクの作成に失敗しました';
         return state;
     }
@@ -39,6 +40,7 @@ export const updateTask = async (id: string, state: FormState, formData: FormDat
         await connectDb();
         await TaskModel.updateOne({_id: id}, updateTask);
     } catch (error) {
+        console.log(error);
         state.error = 'タスクの更新に失敗しました';
         return state;
     }
@@ -52,6 +54,7 @@ export const deleteTask = async (id: string, state: FormState) => {
         await connectDb();
         await TaskModel.deleteOne({_id: id});
     } catch (error) {
+        console.log(error);
         state.error = 'タスクの削除に失敗しました';
         return state;
     }
